f2ba0f2a5498ffbfa6c48628f263c5b48be5a9e8
Gioia Ascari <gioia.ascari96@gmail.com>
2022-03-16 13:25:10 +0100

    correzione esercizio perchè troppo disordinato

--- js/main.js
+++ js/main.js
@@ -9,45 +9,27 @@ In seguito l'utente clicca su una cella: se il numero è presente nella lista de
 La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
  */
+
 //Creo una griglia di gioco quadrata
 const grid = document.getElementById("grid");
 
+const buttonEasy = document.getElementById("easy");
+const buttonMedium = document.getElementById("medium");
+const buttonHard = document.getElementById("hard");
+
 //L'utente indica un livello di difficoltà
 let userDifficult = prompt("Scegli difficoltà tra easy, medium oppure hard");
-while (
-  userDifficult !== "medium" &&
-  userDifficult !== "hard" &&
-  userDifficult !== "easy"
-) {
-  userDifficult = prompt(
-    "Errore, scegli difficoltà tra easy, medium oppure hard"
-  );
-}
+
 //Con If divido le probabilità di gioco
 //Difficoltà easy
 
 if (userDifficult === "easy") {
   for (let i = 1; i <= 100; i++) {
-    //Creo la cella
-    const cell = createCell();
-
-    // aggiungo la cella nella griglia
-    grid.appendChild(cell);
-    function createCell() {
-      const item = document.createElement("div");
-      item.classList.add("cell", "easy");
-      return item;
-    }
-
-    //Aggiungo il colore alla cella quando viene cliccata
-    cell.addEventListener("click", function (event) {
-      cell.classList.toggle("bg-blue");
-    });
-
-    //Distribuzione celle da 1 a 100
     let message = i;
     cell.innerText = message;
     console.log(message);
+    const item = document.createElement("div");
+    item.classList.add("cell", "easy");
   }
 }
 //Difficoltà media
